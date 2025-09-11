1b61a0d8bd626bc1d5352cf9aa42cdeec747a6af
Wevertonf45 <weverton07102005@gmail.com>
2025-09-11 09:15:20 -0300

    Landing page site de tecnologia

---
+++ landing-page/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import HeroSection from './components/HeroSection/HeroSection'
+import NavBar from './components/NavBar/NavBar'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <main>
+        <HeroSection />
+      </main>
+    </>
+  )
+}
+
+export default App
+
+
