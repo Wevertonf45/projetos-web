75204df9c34b2f492280f83542e80cf88d7fe5bc
Wevertonf45 <weverton07102005@gmail.com>
2025-09-15 17:38:32 -0300

    Update Input.jsx

--- sistema-de-login-e-cadastro/src/components/Input/Input.jsx
+++ sistema-de-login-e-cadastro/src/components/Input/Input.jsx
@@ -3,7 +3,7 @@ import './Input.css'
 function Input (props) {
   return (
     <div className="input-container">
-      <input placeholder={props.placeholder} type={props.type} />
+      <input placeholder={props.placeholder} type={props.type} required />
     </div>
   )
 }
