505b5b8e0a91d4c80c788671b87ab3cd8b290445
Wevertonf45 <weverton07102005@gmail.com>
2025-09-22 10:02:49 -0300

    .

--- sistema-de-login-e-cadastro/src/validations/CadastroSchema.js
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup'
-
-const CadastroSchema = yup.object({
-    name: yup.string().required('*O nome é obrigatório!'),
-    email: yup.string().email('*Informe um email válido!').required('*O email é obrigatório'),
-    password: yup.string().min(6, '*A senha deve ter pelo menos 6 caracteres!').required('*A senha é obrigatório!')
-}).required()
-
-export default CadastroSchema
--- sistema-de-login-e-cadastro/src/validations/LoginSchema.js
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup'
-
-const LoginSchema = yup.object({
-    email: yup.string().email('*Informe um email válido!').required('*O email é obrigatório'),
-    password: yup.string().min(6, '*A senha deve ter pelo menos 6 caracteres!').required('*A senha é obrigatória!')
-}).required()
-
-export default LoginSchema
