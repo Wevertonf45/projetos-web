7cd4e7407fc65f8ed1b0d9e37fca0d144e63578c
Wevertonf45 <weverton07102005@gmail.com>
2025-09-26 09:36:22 -0300

    .

--- site-odontologia/src/components/AboutSection/AboutSection.jsx
+++ site-odontologia/src/components/AboutSection/AboutSection.jsx
@@ -2,6 +2,7 @@ import TemplateSection from '../TemplateSection/TemplateSection'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTooth } from '@fortawesome/free-solid-svg-icons';
 import './AboutSection.css'
+import './MediaAbout.css'
 import AboutCard from '../Cards/AboutCard';
 
 function AboutSection() {
@@ -30,9 +31,9 @@ function AboutSection() {
           desc='Excelência Profissional Com formação sólida e constante atualização, oferecemos um atendimento baseado nas técnicas mais modernas da odontologia. Isso garante resultados de alta qualidade e procedimentos seguros, sempre pensados para promover a saúde e a estética do seu sorriso.'
         />
 
-        <AboutCard 
-        title='TREINAMENTOS E SEMINÁRIOS'
-        desc='Nossos treinamentos e seminários são desenvolvidos com base em anos de prática e atualização constante, garantindo conteúdos relevantes e aplicáveis à realidade do mercado. Você aprende com quem realmente entende e já aplicou o que ensina.'
+        <AboutCard
+          title='TREINAMENTOS E SEMINÁRIOS'
+          desc='Nossos treinamentos e seminários são desenvolvidos com base em anos de prática e atualização constante, garantindo conteúdos relevantes e aplicáveis à realidade do mercado. Você aprende com quem realmente entende e já aplicou o que ensina.'
         />
       </div>
     </TemplateSection>
