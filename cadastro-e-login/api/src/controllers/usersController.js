1499a3a6e4d0ddbf2f2be37f2446dd850ffa4e69
Wevertonf45 <weverton07102005@gmail.com>
2025-10-16 09:04:06 -0300

    .

--- cadastro-e-login/api/src/controllers/usersController.js
+++ cadastro-e-login/api/src/controllers/usersController.js
@@ -1,6 +1,9 @@
 //IMportações
 import database from "../configs/database.js"
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const JWT_SECRET = process.env.JWT_SECRET
 
 
 //Rota para cadastro
@@ -43,5 +46,47 @@ export const register = async (req, res) => {
 
 
 //Rota para fazer login
-export const login = (req, res) => {
+export const login = async (req, res) => {
+    try {
+        //Salvar a requisição
+        const { email, password } = req.body
+
+        //Verificar os campos
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Preencha todos os campos!' })
+        }
+
+        //Buscar o usuário pelo email
+        const [rows] = await database.execute(
+            'SELECT * FROM users WHERE email = ?', [email]
+        )
+
+        const user = rows[0]
+
+        //Verificar se o usuário existe
+        if (!user) {
+            return res.status(404).json({ message: 'E-mail não cadastrado!' })
+        }
+
+        //Verifica a senha
+        const isPassword = await bcrypt.compare(password, user.password)
+        if (!isPassword) {
+            return res.status(401).json({ message: 'Senha inválida!' })
+        }
+
+        //Gera o token
+        const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '1h' })
+
+        //Responder
+        return res.status(200).json({
+            message: 'Login efetuado com sucesso!',
+            token,
+            user: { id: user.id, name: user.name, email: user.email },
+        });
+
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ message: 'Erro no servidor!' })
+    }
 }
