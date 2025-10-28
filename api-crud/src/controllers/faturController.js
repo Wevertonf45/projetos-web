5c99d4527c845553ae1e5c7f345f62e4380a0fa2
Wevertonf45 <weverton07102005@gmail.com>
2025-10-28 12:35:47 -0300

    Criei a rota para retornar dados

---
+++ api-crud/src/controllers/faturController.js
@@ -0,0 +1,17 @@
+import connection from '../configs/database.js';
+
+
+//Rota para listar os valores anuais
+export const faturamentoAnual = async (req, res) => {
+    try {
+        //Acessar o banco de dados
+        const [values] = await connection.execute('SELECT * FROM faturamento_anual');
+
+        //Responder
+        return res.status(200).json({ values });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+};
--- api-crud/src/routes.js
+++ api-crud/src/routes.js
@@ -1,3 +1,9 @@
+//Importações
 import express from 'express';
+import { faturamentoAnual } from './controllers/faturController.js';
 
 const router = express.Router();
+
+router.get('/faturamento', faturamentoAnual);
+
+export default router;
